c89a1bbbf132c011fb5abd28b6afa0074bd00069
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-02-15 21:53:04 +1030

    created program functionality

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+for (let i = 1; ; i++) {
+	if (i == 101) {
+		break;
+	}
+	else if ((i % 15) == 0) {
+		console.log("FizzBuzz");
+	}
+	else if ((i % 5) == 0) {
+		console.log("Buzz");
+	}
+	else if ((i % 3) == 0) {
+		console.log("Fizz");
+	}
+	else console.log(i);
+}
